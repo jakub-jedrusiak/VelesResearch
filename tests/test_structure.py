5f5bd40d7bd67410544d844d523c5c6b295d89e5
Jakub Jędrusiak <kuba23031999@gmail.com>
2023-06-10 23:18:53 +0200

    additional tests

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -12,6 +12,15 @@ def test_calling():
     st.Survey(st.Page("test", st.Question("q", "radio", "Question 1", "Yes", "No")))
 
 
+def test_printing():
+    "Test printing all classes"
+    print(st.Question("q", "radio", "Question 1", "Yes", "No"))
+    print(st.Page("test", st.Question("q", "radio", "Question 1", "Yes", "No")))
+    print(
+        st.Survey(st.Page("test", st.Question("q", "radio", "Question 1", "Yes", "No")))
+    )
+
+
 def test_unique_labels_questions():
     "Test that exception is raised when questions labels in page are not unique"
     with pytest.raises(ValueError):
@@ -25,3 +34,19 @@ def test_unique_labels_pages():
         question = st.Question("q", "radio", "Question 1", "Yes", "No")
         page = st.Page("test", question)
         st.Survey(page, page)
+
+
+def test_subscripting_page():
+    "Test subscripting in Page objects"
+    page = st.Page("test", st.Question("q", "radio", "Question 1", "Yes", "No"))
+    print(page[0])
+    print(page["q"])
+
+
+def test_subscripting_survey():
+    "Test subscripting in Survey objects"
+    survey = st.Survey(
+        st.Page("test", st.Question("q", "radio", "Question 1", "Yes", "No"))
+    )
+    print(survey[0])
+    print(survey["test"])
