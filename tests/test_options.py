b7516ed326b1cc0faaaf8884b675f38d7f90ba0a
Jakub Jędrusiak <kuba23031999@gmail.com>
2023-06-11 19:59:48 +0200

    survey options test

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,6 +1,8 @@
 "Test options.py module"
 
+from os import remove
 import velesresearch.options as opt
+from velesresearch.tools import question, page, survey
 
 
 def test_calling():
@@ -8,3 +10,15 @@ def test_calling():
     opt.QuestionOptions()
     opt.PageOptions()
     opt.SurveyOptions()
+
+
+def test_survey_opts():
+    q_opt = opt.QuestionOptions(required=True)
+    p_opt = opt.PageOptions(visible=False)
+    s_opt = opt.SurveyOptions(language="pl")
+
+    q = question("q1", "radio", "What is life?", "Yes", "No", "42", options=q_opt)
+    p = page("p1", q, options=p_opt)
+    s = survey(p, options=s_opt)
+
+    remove("survey.json")
