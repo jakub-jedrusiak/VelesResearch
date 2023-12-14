cb8b5ecf75ab8e771b277f7f1bde707c10d09d79
Jakub Jędrusiak <kuba23031999@gmail.com>
2023-12-14 09:39:44 +0100

    i18n libraries

--- velesresearch/website_template/src/SurveyComponent.jsx
+++ velesresearch/website_template/src/SurveyComponent.jsx
@@ -1,5 +1,6 @@
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
+import "survey-core/survey.i18n";
 import "survey-core/defaultV2.min.css";
 import { json } from "./survey.js";
 
