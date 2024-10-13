b2c869e18d51b7b8612e844c44876f96e4281b4a
Jakub Jędrusiak <kuba23031999@gmail.com>
2024-10-13 14:49:18 +0200

    Added css file

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,9 @@
 # Release notes
 
+## future release
+
+* Added a functional `index.css` file.
+
 ## 0.2.0
 
 * `yarn` is now called directly through `subprocess.run()`. This should resolve some problems in Windows. `pynpm` dependency has been removed.
--- velesresearch/website_template/src/index.js
+++ velesresearch/website_template/src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import SurveyComponent from "./SurveyComponent";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
